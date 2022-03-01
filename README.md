546c59c3e1c3eca848b355339125e1f2b582c6dd
xNegis <xnegis@gmail.com>
2022-03-01 21:58:48 +0100

    Update readme

--- README.md
+++ README.md
@@ -1,18 +1,17 @@
-# Getting Started with Create React App
+# Lanzar el proyecto
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+Para lanzar el proyecto debemos tener instalado `Node.js` y `npm`
+https://phoenixnap.com/kb/install-node-js-npm-on-windows
 
-## Available Scripts
+Una vez instaladas dichas herramientas, y clonado el repostorio, en la carpeta raíz del repositorio corremos
+`npm install`
 
-In the project directory, you can run:
+ y después `npm start`
 
-### `npm start`
+Esto correrá la app en modo desarrollador
+Open [http://localhost:3000](http://localhost:3000)
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
-
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
+Abajo dejo otra documentación oficial
 
 ### `npm test`
 
--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Home from './Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import ClientList from './Client/ClientList';
+import ClientEdit from "./Client/ClientEdit";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+        <Router>
+          <Switch>
+            <Route path='/' exact={true} component={Home}/>
+            <Route path='/clients' exact={true} component={ClientList}/>
+            <Route path='/clients/:id' component={ClientEdit}/>
+          </Switch>
+        </Router>
+    )
+  }
+
+  
 }
-
 export default App;
