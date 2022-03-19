fd72c53b93cc76d37a2a367d847b993aeec88554
Félix Conde Marrón <48648167+felconmar@users.noreply.github.com>
2022-03-19 12:17:14 +0100

    #4 : Carga desde GeoJSON y Popups
    Los popups no funcionan, pero se cargan bien las propiedades.

--- src/components/react-leaflet.js
+++ src/components/react-leaflet.js
@@ -3,7 +3,21 @@ import { MapContainer, TileLayer, useMap } from "react-leaflet";
 import 'leaflet/dist/leaflet.css';  
 import { Marker } from 'react-leaflet';
 import {  iconLocation  } from './IconLocation';
+import L from 'leaflet'
+import garages from '../assets/plazas_ejemplo.json'
 
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+    iconUrl: require('../resources/logoSinFondo.png'),
+    iconRetinaUrl: require('../resources/logoSinFondo.png'),
+    iconAnchor: null,
+    popupAnchor: null,
+    shadowUrl: null,
+    shadowSize: null,
+    shadowAnchor: null,
+    iconSize: new L.point(90, 90),
+});
 
 function LocationMarker() {
     const [position, setPosition] = useState(null);
@@ -16,6 +30,19 @@ function LocationMarker() {
         setPosition(e.latlng);
         map.flyTo(e.latlng, map.getZoom());
         setBbox(e.bounds.toBBoxString().split(","));
+
+        const parksGeoJson = new L.GeoJSON(garages, {
+          onEachFeature: (feature = {} , layer) => {
+            const { properties = {} } = feature;
+            const { Name } = properties;
+            //if (!Name ) return;
+            console.log("Name:");
+            console.log(Name);
+            layer.bindPopup('<p>aaaaaaaaaaaaaaaaaaaaaa</p>');
+          }
+        });
+
+        parksGeoJson.addTo(map);
       });
     }, [map]);
 
@@ -39,4 +66,4 @@ const MapView = () => {
     );
 };
 
-export default MapView;
+export default MapView
