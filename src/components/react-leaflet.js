adac188f35cf833f1f1af0a5d1729481caf61171
mangarmar <mangarmar17@alum.us.es>
2022-03-20 17:33:54 +0100

    #4: Se ha creado la adición dinámica de plazas de garaje

--- src/components/react-leaflet.js
+++ src/components/react-leaflet.js
@@ -5,6 +5,8 @@ import { Marker } from 'react-leaflet';
 import {  iconLocation  } from './IconLocation';
 import L from 'leaflet'
 import garages from '../assets/plazas_ejemplo.json'
+import axios from 'axios';
+import { map } from 'leaflet';
 
 delete L.Icon.Default.prototype._getIconUrl;
 
@@ -19,19 +21,30 @@ L.Icon.Default.mergeOptions({
     iconSize: new L.point(90, 90),
 });
 
-let prueba_USA = "https://www.nps.gov/lib/npmap.js/4.0.0/examples/data/national-parks.geojson";
+let prueba = 'https://docs.mapbox.com/mapbox-gl-js/assets/earthquakes.geojson';
+
+
 
 function LocationMarker() {
     const [position, setPosition] = useState(null);
     const [bbox, setBbox] = useState([]);
 
     const map = useMap();
-
+  
     useEffect(() => {
       map.locate().on("locationfound", function (e) {
         setPosition(e.latlng);
         map.flyTo(e.latlng, map.getZoom());
         setBbox(e.bounds.toBBoxString().split(","));
+
+        fetch(
+          prueba
+          ).then(
+            res => res.json()
+          ).then(
+            data => L.geoJSON(data).addTo(map)
+          ).catch(error => console.error('Error:', error))
+
 
         const parksGeoJson = new L.GeoJSON(garages, {
           onEachFeature: (feature = {} , layer) => {
@@ -40,7 +53,6 @@ function LocationMarker() {
             layer.bindPopup('<h1>' + properties.Precio_hora + ' €' + '</h1> <a href="/reserva-plaza"><button>Reservar plaza</button></a>');
           }
         });
-
         parksGeoJson.addTo(map);
       });
     }, [map]);
