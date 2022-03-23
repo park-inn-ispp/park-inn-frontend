70c37894414ef8c9f861129fe2123263a08d2d37
vicdiacor <vicdiacor@alum.us.es>
2022-03-23 03:00:59 +0100

    "#13": Cambio de endpoints en produccion
    Se han modificados las llamadas a la API para que funcionen mediante heroku, en lugar de en local

--- src/Plaza/CreatePlaza.js
+++ src/Plaza/CreatePlaza.js
@@ -53,11 +53,11 @@ export default function CreatePlaza() {
       
       const requestOptions = {
         method: 'POST',
-        headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin' : 'http://localhost:3000', "mode": "cors"},
+        headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin' : 'https://park-inn-ispp-fe.herokuapp.com/', "mode": "cors"},
         body: (JSON.stringify(data))
       };
       
-      fetch('http://localhost:8080/plazas/', requestOptions)
+      fetch('https://park-inn-ispp-be.herokuapp.com/plazas/', requestOptions)
         .then(response => {
           console.log(response.ok)
           if (response.ok){
