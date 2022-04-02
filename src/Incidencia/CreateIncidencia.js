f3eb51bc482ba8dfb888f7cd93dffd49dfc93d74
Félix Conde Marrón <48648167+felconmar@users.noreply.github.com>
2022-04-02 13:55:39 +0200

    #50 : Incluye las etiquetas de formulario
    Se han incluido estas etiquetas al formulario

--- src/Incidencia/CreateIncidencia.js
+++ src/Incidencia/CreateIncidencia.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import ValidateIncidenciaForm from './ValidateIncidenciaForm';
 import { useParams, useNavigate } from 'react-router-dom';
 import FormErrorMessage from '../Util/FormErrorMessage';
+import {Etiqueta, Parrafo, Formulario} from '../Plaza/ReservaPlaza.elements';
+import { ReactNotifications } from 'react-notifications-component'
 import call from '../Util/Caller';
 import Cookies from 'universal-cookie';
 const cookies = new Cookies();
@@ -10,7 +12,6 @@ const email = cookies.get('user_mail');
 export default function CreateIncidencia() {
     const reservaId = parseInt(useParams().id)
     let navigate = useNavigate();
-  
     const [form, setForm]= useState({
         fecha: "",
         titulo: "",
@@ -18,17 +19,13 @@ export default function CreateIncidencia() {
         idUsuario: "",
         idReserva: "",
     })
-
     const[errors, setErrors]= useState({})
-  
     const handleSubmit= evt => {
         evt.preventDefault()
         var nuevosErrores= ValidateIncidenciaForm(form)
         setErrors(nuevosErrores)
-
         var numeroErrores = Object.keys(nuevosErrores).length;
         if(numeroErrores===0){
-
             const data= {
             "fecha": new Date(),
             "titulo": form.titulo,
@@ -43,39 +40,35 @@ export default function CreateIncidencia() {
                 }
             })
         }
-    }  
+    }
     
     const handleChange= evt => {
-    
-    const target = evt.target
-    const name = target.name
-    var value= target.value
-    
-    setForm({...form,[name]: value})
-    
-    } 
+      const target = evt.target
+      const name = target.name
+      var value= target.value
+      setForm({...form,[name]: value})
+    }
 
   return (
     
   <div className="form-style-10">
-  <h1>Crear incidencia</h1>
-    <form onSubmit={handleSubmit}>
-
-    <div className="inner-wrap">
-      <label>
-      Título:
-        <input onChange={handleChange} name= "titulo" type="text" value={form.titulo} />
-        <FormErrorMessage jsonErrors={errors} errorName="titulo"/>
-      </label>
-      <br/>
-      <label>
-      Descripción:
-        <textarea  onChange={handleChange} name= "descripcion" value={form.descripcion}/>
-        <FormErrorMessage jsonErrors={errors} errorName="descripcion"/>
-      </label>
+    <h1>Crear incidencia</h1>
+    <ReactNotifications />
+    <Formulario onSubmit={handleSubmit}>
+      <div class="inner-wrap">
+        <Etiqueta>Título:</Etiqueta>
+        <Parrafo>
+          <input onChange={handleChange} name= "titulo" type="text" value={form.titulo}/>
+          <FormErrorMessage jsonErrors={errors} errorName="titulo"/>
+        </Parrafo>
+        <Etiqueta>Descripción:</Etiqueta>
+        <Parrafo>
+          <textarea onChange={handleChange} name= "descripcion" value={form.descripcion}/>
+          <FormErrorMessage jsonErrors={errors} errorName="descripcion"/>
+        </Parrafo>
       </div>
-      <input type="submit" value="Crear incidencia" />
-    </form>
+      <input type="submit" value="Crear incidencia"/>
+    </Formulario>
   </div>
   );
 }
