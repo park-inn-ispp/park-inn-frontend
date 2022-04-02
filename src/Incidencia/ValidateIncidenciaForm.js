2bb42bd0faa80d390c7fff3748570aeb9bd3aa11
Félix Conde Marrón <48648167+felconmar@users.noreply.github.com>
2022-04-02 14:12:14 +0200

    #50 : Incluye otros cambios para Codacy

--- src/Incidencia/ValidateIncidenciaForm.js
+++ src/Incidencia/ValidateIncidenciaForm.js
@@ -1,16 +1,21 @@
 export default function ValidateIncidenciaForm(form){
     let errors = {};
     // Validación título
-    if(!form.titulo.trim())
+    if(!form.titulo.trim()){
         errors.titulo= "El título es un campo obligatorio";
-    if (form.titulo.length > 100 )
+    }
+    if (form.titulo.length > 100 ) {
         errors.titulo= "El título no puede contener más de 100 caracteres";
+    }
     // Validación descripcion
-    if(!form.descripcion.trim())
+    if(!form.descripcion.trim()){
         errors.descripcion= "La descripción es un campo obligatorio";
-    if(form.descripcion.length < 100)
+    }
+    if(form.descripcion.length < 100){
         errors.descripcion = "La decripción no puede contener menos de 100 caracteres";
-    if (form.descripcion.length > 500)
+    }
+    if (form.descripcion.length > 500){
         errors.descripcion = "La decripción no puede contener más de 500 caracteres";
+    }
     return errors;
 }
