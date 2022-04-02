ea81941ec605779d1ee09b4234ecea0ed172861d
Félix Conde Marrón <48648167+felconmar@users.noreply.github.com>
2022-04-02 14:04:18 +0200

    #50 : Arregla problemas con Codacy

--- src/Incidencia/ValidateIncidenciaForm.js
+++ src/Incidencia/ValidateIncidenciaForm.js
@@ -1,22 +1,16 @@
 export default function ValidateIncidenciaForm(form){
-
-    let errors = {}
-   
+    let errors = {};
     // Validación título
-    if(!form.titulo.trim()){
-        errors.titulo= "El título es un campo obligatorio" 
-    } else if (form.titulo.length > 100 ){
-        errors.titulo= "El título no puede contener más de 100 caracteres"
-    }
-
+    if(!form.titulo.trim())
+        errors.titulo= "El título es un campo obligatorio";
+    if (form.titulo.length > 100 )
+        errors.titulo= "El título no puede contener más de 100 caracteres";
     // Validación descripcion
-    if(!form.descripcion.trim()){
-        errors.descripcion= "La descripción es un campo obligatorio" 
-    } else if(form.descripcion.length < 100){
-        errors.descripcion = "La decripción no puede contener menos de 100 caracteres"
-    } else if (form.descripcion.length > 500){
-        errors.descripcion = "La decripción no puede contener más de 500 caracteres"
-    }
-
-    return errors
+    if(!form.descripcion.trim())
+        errors.descripcion= "La descripción es un campo obligatorio";
+    if(form.descripcion.length < 100)
+        errors.descripcion = "La decripción no puede contener menos de 100 caracteres";
+    if (form.descripcion.length > 500)
+        errors.descripcion = "La decripción no puede contener más de 500 caracteres";
+    return errors;
 }
