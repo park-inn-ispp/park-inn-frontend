9f25dbe0aba7c49b48b4a24fa0f15ac6003ece05
luibelzan <luismi_madrilista@hotmail.com>
2022-04-04 20:04:49 +0200

    #47: Botones de aceptar y rechazar reserva añadidos

--- src/Reservas/ReservasOwnerList.js
+++ src/Reservas/ReservasOwnerList.js
@@ -1,10 +1,16 @@
 import ListComponent from "../components/ListComponent";
 import call from "../Util/Caller";
 import {useEffect, useState} from "react";
-import {useParams} from "react-router-dom";
+import { useParams, Navigate, useNavigate } from 'react-router-dom';
 import Loading from "../components/Loading";
+import Cookies from 'universal-cookie';
+
+const cookies = new Cookies();
+
 
 export default function ReservaOwnerList(){
+    let navigate = useNavigate();
+
     const id = useParams().id;
     const [reservas, setReservas] = useState(['loading']);
      
@@ -14,6 +20,7 @@ export default function ReservaOwnerList(){
         .then(response => response.json())
         .then((res) => setReservas(res));
     },[id]);
+
     console.log(reservas);
     if (reservas[0] === 'loading'){
         return(
@@ -25,15 +32,60 @@ export default function ReservaOwnerList(){
             <h2>Esta plaza no existe o no está disponible en este momento</h2>
         )
     }
+
+    function aceptarReserva(reservaId) {
+        call(`/reservas/`+reservaId+'/aceptar', 'GET')
+          .then(response => {
+
+    
+            if (response.ok){
+              navigate(`/mis-reservas-de-mis-plazas/plaza/`+id)
+            }
+          })
+      }
+
+      function rechazarReserva(reservaId) {
+        call(`/reservas/`+reservaId+'/rechazar', 'GET')
+          .then(response => {
+            console.log(response)
+    
+            if (response.ok){
+              navigate(`/mis-reservas-de-mis-plazas/plaza/`+id)
+            }
+          })
+      }
+
     return(
-        <div>
-            <h1>Reservas</h1>
-            <ListComponent
-                header={"id"} 
-                data={reservas} 
-                attributes={[{position:1,val:'estado'},{position:2,val:'fechaInicio'},{position:3,val:'fechaFin'},{position:4,val:'fechaSolicitud'},{position:5,val:'precioTotal'}]} 
-                headers={['Estado', 'Fecha Inicio', 'Fecha Fin', "Fecha Solicitud", "Precio"]} 
-            />
+        <body>
+        <div className="tablas">
+            <table>
+                <tr>
+                    <th>Cliente</th>
+                    <th>Fecha Inicio</th>
+                    <th>Fecha Fin</th>
+                    <th>Precio total</th>
+                    <th>Estado</th>
+                </tr>
+                {reservas.map((reserva) => {
+                    var estadoReserva = reserva.estado=="pendiente";
+                    var cancalacionReserva = reserva.estadoo=="aceptada" || reserva.estado=="rechazada";
+                    return <tr>
+                        <td>{reserva.user.name}</td>
+                        <td>{reserva.fechaInicio}</td>
+                        <td>{reserva.fechaFin}</td>
+                        <td>{reserva.precioTotal}</td>
+                        <td>
+                        {
+                    estadoReserva ? (
+                        <><button type='button' class='deleteButton' onClick={() => rechazarReserva(reserva.id)}>Rechazar reserva</button><button type='button' class='editButton' onClick={() => aceptarReserva(reserva.id)}>Aceptar reserva</button></>
+
+                    ) : (reserva.estado)}
+                        </td>
+                    </tr>
+                })}
+            </table>
         </div>
+        </body>
+        
     );
 }
