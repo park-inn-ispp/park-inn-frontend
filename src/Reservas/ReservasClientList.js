5ec0106aeac84fa26f1ae063445b48ee398db687
Manuel Carnero Vergel <73230495+mancarver1@users.noreply.github.com>
2022-04-05 04:19:40 +0200

    Update ReservasClientList.js

--- src/Reservas/ReservasClientList.js
+++ src/Reservas/ReservasClientList.js
@@ -1,34 +1,85 @@
-import React, { Component } from 'react';
-import ListComponent from '../components/ListComponent'
-import call from '../Util/Caller';
+import ListComponent from "../components/ListComponent";
+import call from "../Util/Caller";
+import {useEffect, useState} from "react";
+import { useParams, Navigate, useNavigate } from 'react-router-dom';
+import Loading from "../components/Loading";
 import Cookies from 'universal-cookie';
 
 const cookies = new Cookies();
 
-class ReservasList extends Component{
-    constructor(props){
-        super(props);
-        this.state = {reservas: []};
-    }
 
-    componentDidMount(){
-        const usuario = cookies.get('UserData');
-        call(`/reservas/usuario/`+usuario.id,"GET")
+
+export default function ReservaClientList(){
+    let navigate = useNavigate();
+
+    const id = useParams().id;
+    const [reservas, setReservas] = useState(['loading']);
+     
+    console.log(id);
+    useEffect(() => {
+        call(`/mis-reservas/usuario/${id}`,"GET")
         .then(response => response.json())
-        .then((data) => {
-            this.setState({reservas: data})
-        });
+        .then((res) => setReservas(res));
+    },[id]);
+
+    console.log(reservas);
+    if (reservas[0] === 'loading'){
+        return(
+            <Loading></Loading>
+        )
+    }
+    if (reservas === 'undefined' || reservas.length === 0){
+        return(
+            <h2>No hay reservas asociadas a este usuario</h2>
+        )
     }
 
-    render(){
-        return(
-            <ListComponent
-                header={"fechaSolicitud"}
-                data={this.state.reservas}
-                attributes={[{position:1,val:'id'},{position:2,val:'fechaInicio'},{position:3,val:'fechaFin'}, {position:4,val:'plaza.direccion'}]} 
-                headers={['id', 'fechaInicio', 'fechaFin', 'direccion']}
-                />
-        );
-    }
+   
+      function cancelarReserva(reservaId) {
+        call(`/reservas/`+reservaId+'/cancelar', 'GET')
+          .then(response => {
+            console.log(response)
+    
+            if (response.ok){
+              navigate(`/mis-reservas`)
+            }
+          })
+      }
+
+    return(
+        <body>
+        <div className="tablas">
+            <table>
+                <tr>
+                    <th>Propietario</th>
+                    <th>Direccion</th>
+                    <th>Fecha Inicio</th>
+                    <th>Fecha Fin</th>
+                    <th>Precio total</th>
+                    <th>Estado</th>
+                </tr>
+                {reservas.map((reserva) => {
+                    //var estadoReserva = reserva.estado=="pendiente";
+                    var cancelacionReserva = reserva.estado=="aceptada" || reserva.estado=="rechazada";
+                    return <tr>
+                        <td>{reserva.plaza.administrador.name}</td>
+                        <td>{reserva.plaza.direccion}</td>
+
+                        <td>{reserva.fechaInicio}</td>
+                        <td>{reserva.fechaFin}</td>
+                        <td>{reserva.precioTotal}</td>
+                        <td>
+                        {
+                    cancelacionReserva ? (
+                        <><button type='button' class='deleteButton' onClick={() => cancelarReserva(reserva.id)}>Cancelar reserva</button></>
+
+                    ) : (reserva.estado)}
+                        </td>
+                    </tr>
+                })}
+            </table>
+        </div>
+        </body>
+        
+    );
 }
-export default ReservasList;
