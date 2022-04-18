dc2bd2118b00649694e5478686771f79d4c05046
mangarmar <mangarmar17@alum.us.es>
2022-04-18 19:31:24 +0200

    #106: Primera versión filtrado por precio hora

--- src/components/react-leaflet.js
+++ src/components/react-leaflet.js
@@ -6,8 +6,6 @@ import {  iconLocation  } from './IconLocation';
 import L from 'leaflet';
 import GeoJSON from 'geojson';
 import call from '../Util/Caller';
-import FilterComponent from "./FilterComponent";
-
 delete L.Icon.Default.prototype._getIconUrl;
 
 L.Icon.Default.mergeOptions({
@@ -21,8 +19,6 @@ L.Icon.Default.mergeOptions({
     iconSize: new L.point(90, 90),
 });
 
-const frameworks = ["ReactJS", "AngularJS", "VueJS", "NodeJS", "EmberJS"];
-
 function LocationMarker() {
     const [position, setPosition] = useState(null);
     const [bbox, setBbox] = useState([]);
@@ -30,36 +26,8 @@ function LocationMarker() {
 
     const map = useMap();
 
-    useEffect(() => {
-      const Plazas = async () => {
-          const data = await call('/plazas/all', 'GET');
-          const plazas = await data.json()
-          setPlazas(plazas);
-      }
-      Plazas();
-      function onLocationFound(e) {
 
-        L.marker(e.latlng, {icon: iconLocation}).addTo(map) 
-      }
-     
-       map.on('locationfound', onLocationFound);    
-  }, []);
-
-
-  map.locate({setView: true, maxZoom: 16});
     
-  let dataFilter = plazas.filter(p => p.precioHora > 10);
-
-  let geojson = GeoJSON.parse(dataFilter, {Point: ['latitud', 'longitud'],include: ['id', 'precioHora', 'fianza']});
-
-  L.geoJSON(geojson, {
-    onEachFeature: function(feature, layer){
-      layer.bindPopup('<h5> Precio por hora: ' + layer.feature.properties.precioHora + ' €' + '</h5> <a href="/reservas/plaza/'+layer.feature.properties.id+'"><button>Reservar plaza</button></a>')
-    }
-  }).addTo(map)
-  console.log(geojson)
-    
-  /*
     useEffect(() => {
       map.locate().on("locationfound", function (e) {
         setPosition(e.latlng);
@@ -70,61 +38,72 @@ function LocationMarker() {
           .then(
             data => GeoJSON.parse(data, {Point: ['latitud', 'longitud'],include: ['id', 'precioHora', 'fianza']})
           ).then(data => {
-            L.geoJSON(data, {
+
+            var menosDe2 = L.geoJSON(data, { 
               onEachFeature: function(feature, layer){
                 layer.bindPopup('<h5> Precio por hora: ' + layer.feature.properties.precioHora + ' €' + '</h5> <a href="/reservas/plaza/'+layer.feature.properties.id+'"><button>Reservar plaza</button></a>')
-              }
-            }).addTo(map)
-          }
-          ).catch(error => console.error("Error: ",error));
+              },
+              filter: function(feature) { return (feature.properties.precioHora > 0 && feature.properties.precioHora <=1.99) }
+            });
+        
+            var entre2Y5 = L.geoJSON(data, { 
+              onEachFeature: function(feature, layer){
+                layer.bindPopup('<h5> Precio por hora: ' + layer.feature.properties.precioHora + ' €' + '</h5> <a href="/reservas/plaza/'+layer.feature.properties.id+'"><button>Reservar plaza</button></a>')
+              },
+              filter: function(feature) { return (feature.properties.precioHora > 1.99 && feature.properties.precioHora <=5.00) }
+            });
+        
+            var masDe5 = L.geoJSON(data, { 
+              onEachFeature: function(feature, layer){
+                layer.bindPopup('<h5> Precio por hora: ' + layer.feature.properties.precioHora + ' €' + '</h5> <a href="/reservas/plaza/'+layer.feature.properties.id+'"><button>Reservar plaza</button></a>')
+              },
+              filter: function(feature) { return feature.properties.precioHora > 5.00 }
+            });
+
+            var todas = L.geoJSON(data, { 
+              onEachFeature: function(feature, layer){
+                layer.bindPopup('<h5> Precio por hora: ' + layer.feature.properties.precioHora + ' €' + '</h5> <a href="/reservas/plaza/'+layer.feature.properties.id+'"><button>Reservar plaza</button></a>')
+              },
+            });
+        
+        
+            var baseMaps = {
+              "Todo": todas,
+          };
+          
+          var overlayMaps = {
+              "Menos 2€": menosDe2,
+              "2€-5€": entre2Y5,
+              "Mas 5€": masDe5
+          };
+
+          L.control.layers(baseMaps, overlayMaps,{
+            position: 'topright', // 'topleft', 'bottomleft', 'bottomright'
+            collapsed: true // true
+        }).addTo(map);
+        }).catch(error => console.error("Error: ",error));
 
       });
     }, [map]);
-*/
 
-const [selectedFrameworks, setSelectedFrameworks] = useState([]);
- 
-const handleSelect = framework => {
-const isSelected = selectedFrameworks.includes(framework);
-const newSelection = isSelected
-? selectedFrameworks.filter(currentTech => currentTech !== framework)
-: [...selectedFrameworks, framework];
-setSelectedFrameworks(newSelection);};
 
-  return(
-    <>
-    < FilterComponent label="JS Frameworks" onSelect={() => alert(selectedFrameworks)} >
-              < div className="frameworks-list" >
-                  {frameworks.map((framework, index) => {
-                      const isSelected = selectedFrameworks.includes(framework);
-                          return (
-                              < label key={index} >
-                                  < input type="checkbox" checked={isSelected} onChange={() => handleSelect(framework)}/>
-                                  < span className="ml-2 text-base text-gray-500 font-heading" >
-                                      {framework}
-                                  </span >
-                              </label >   
-                          ); 
-                  })}
-              </div >
-  </FilterComponent >
-  
-  </>
-  );
+    return position === null ? null : (
+      <Marker position={position} icon={iconLocation}>
+      </Marker>
+    );
   }
 
 
 const MapView = () => {
-
+  const position = [51.505, -0.09]
     return(
-      <MapContainer center = {{lat: '37.359555663066864', lng: '-5.98625077322988'}} zoom={8}>
-          <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-       />
-       <LocationMarker />
-      </MapContainer>   
-      
+    <MapContainer center={position} zoom={15}>
+            <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+         />
+      <LocationMarker />
+    </MapContainer>   
     );
 };
 
