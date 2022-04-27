8416668a1a69c8f5ea4320cbda7e53d6182799d9
Fernando Andrés <fandresfernanduelo@gmail.com>
2022-04-27 15:52:46 +0200

    #111 Hotfix/CSS-Tramos
    Arreglado fallos coordinacion de ramas

--- src/components/CardReservas.js
+++ src/components/CardReservas.js
@@ -1,86 +1,24 @@
 import React from "react";
-import PropTypes from "prop-types";
-import call from "../Util/Caller";
-function cancelarReserva(reservaId) {
-
-  call(`/reservas/`+reservaId+'/cancelar', 'GET')
-    .then(response => {
-      console.log(response)
-
-      if (response.ok){
-        //navigate(`/mis-reservas`)
-        window.location.reload()
-      }
-    })
-}
+import { Principal, Tarjeta, Datos } from "./card.elements";
 
 
-function CardReservas({ imageSource,id, title,propietario,fechaInicio,fechaFin,precioTotal, estado, urlCancelar,urlDetalles }) {
+export default function CardReservas({id, direccion, fechaInicio, fechaFin, precioTotal, estado}) {
   let canceledDiv =  <div className="cancelada"></div>
-  let pendentDiv =  <div className="pendiente"></div>
+  let pendenTDiv =  <div className="pendiente"></div>
   let aceptedDiv =  <div className="aceptada"></div>
-
   return (
-    <div className="card text-center bg-dark animate__animated animate__fadeInUp">
-
-      <div className="card-body text-light">
-        <h4 className="card-title">{title}</h4>
-        <div className="card-text-div">
-        <p className="card-text text-secondary">
-          {(estado==='cancelada' || estado==='rechazada')
-            ? canceledDiv
-            : (estado==='pendiente')
-              ? pendentDiv
-              : aceptedDiv
-              }
-        </p>
-        <p className="card-text text-secondary">
-          {propietario
-            ? "Propietario : " + propietario 
-            : "Propietario : -"}
-        </p>
-        <p className="card-text text-secondary">
-          {fechaInicio
-            ? "Fecha Inicio : " + fechaInicio 
-            : "Fecha Inicio : -"}
-        </p>
-        <p className="card-text text-secondary">
-          {fechaFin
-            ? "Fecha Fin : " + fechaFin 
-            : "Fecha Fin : -"}
-        </p>
-        <p className="card-text text-secondary">
-          {precioTotal
-            ? "Precio total : " + precioTotal +"€"
-            : "Precio total : -"}
-        </p>
-        
-        <a
-          onClick={() => cancelarReserva(id)}
-          className="btn btn-outline-secondary border-0"
-          rel="noreferrer"
-        >
-         Cancelar
-
-        </a>
-        <a
-          href={urlDetalles ? urlDetalles : "#!"}
-          className="btn btn-outline-secondary border-0"
-          rel="noreferrer"
-        >
-          Detalles
-        </a>
-        </div>
-      </div>
-    </div>
+    <Principal>
+      <Tarjeta to={`/reservas/${id}`}>
+        <Datos>Dirección: {direccion}</Datos>
+        <Datos>Fecha Inicio: {fechaInicio}</Datos>
+        <Datos>Fecha Fin: {fechaFin}€</Datos>
+        <Datos>Precio Total: {precioTotal}</Datos>
+        <Datos>{estado==="aceptada" ? 
+                aceptedDiv
+                : estado==="cancelada" ?
+                canceledDiv
+                :pendenTDiv}</Datos>
+      </Tarjeta>
+    </Principal>
   );
 }
-
-CardReservas.propTypes = {
-  title: PropTypes.string.isRequired,
-  text: PropTypes.string,
-  url: PropTypes.string,
-  imageSource: PropTypes.string
-};
-
-export default CardReservas;
--- src/components/Leyenda.js
+++ src/components/Leyenda.js
@@ -1,10 +1,20 @@
+import styled from "styled-components";
 import "./leyenda.css";
+
+const Leyend = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  align-items: center;
+  justify-content: center;
+  padding: 10px;
+`;
 
 function Leyenda() {
     
   
     return (
-      <div className="containerLeyenda">
+      <Leyend>
           <div>
             <div className="texto">Aceptada  </div>
             <div className="a"></div>
@@ -18,7 +28,7 @@ function Leyenda() {
             <div className="c"></div>
           </div>
 
-      </div>
+      </Leyend>
     ); 
   }
 
