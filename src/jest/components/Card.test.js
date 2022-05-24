9a534c8c77ff5be9c39be45b861235a3ae854bcd
xNegis <xnegis@gmail.com>
2022-05-24 13:58:15 +0200

    tests fixed

--- src/jest/components/Card.test.js
+++ src/jest/components/Card.test.js
@@ -11,7 +11,7 @@ test('Renderizar una Card vacía', () => {
 
     const emptyCard = shallow(<Card />);
 
-    expect(emptyCard.text()).toEqual('DIRECCIÓN: DIMENSIONES: m x mPRECIO: €EXTERIOR: EditarReservasEliminar');
+    expect(emptyCard.text()).toEqual('PRECIO: €EditarReservasEliminar');
   
 });
 
@@ -19,7 +19,7 @@ test('Renderizar una Card no vacía', () => {
 
     const notEmptyCard = shallow(<Card  title={"test 1 title"} ancho={"test 1 ancho"} largo={"test 1 largo"} precioHora={"test 1 precioHora"} esAireLibre={"test 1 esAireLibre"} urlEdit={"test 1 urlEdit"} urlReserva={"test 1 urlReserva"}/>);
 
-    expect(notEmptyCard.text()).toEqual("DIRECCIÓN: test 1 titleDIMENSIONES: test 1 anchom x test 1 largomPRECIO: test 1 precioHora€EXTERIOR: test 1 esAireLibreEditarReservasEliminar");
+    expect(notEmptyCard.text()).toEqual("test 1 titlePRECIO: test 1 precioHora€EditarReservasEliminar");
   
 });
 
@@ -28,6 +28,6 @@ test('Renderizar una Card no vacía sin titulo', () => {
 
     const notEmptyCard = shallow(<Card  ancho={"test 1 ancho"} largo={"test 1 largo"} precioHora={"test 1 precioHora"} esAireLibre={"test 1 esAireLibre"} urlEdit={"test 1 urlEdit"} urlReserva={"test 1 urlReserva"}/>);
 
-    expect(notEmptyCard.text()).toEqual("DIRECCIÓN: DIMENSIONES: test 1 anchom x test 1 largomPRECIO: test 1 precioHora€EXTERIOR: test 1 esAireLibreEditarReservasEliminar");
+    expect(notEmptyCard.text()).toEqual("PRECIO: test 1 precioHora€EditarReservasEliminar");
   
 });
